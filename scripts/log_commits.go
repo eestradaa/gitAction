c4d8a7cf4d033177fc553bc00344901bc73bb1fa
eestradaa <85235191+eestradaa@users.noreply.github.com>
2025-05-16 16:39:28 +0200

    Create log_commits.go

---
+++ scripts/log_commits.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "os/exec"
+  "path/filepath"
+  "time"
+)
+
+func main() {
+  cmd := exec.Command("git", "log", "-n", "3", "--pretty-format:%h - %an : %s")
+  out, err := cmd.Output()
+  if err != nil {
+    fmt.Printf("Error ejecutando git log %v\n", err)
+    os.Exit(1)
+  }
+
+  // Creacio de la carpeta log
+  logDir := filepath.Join("..", "log")
+  if _, err := os.Stat(logDir); os.IsNotExist(err) {
+    //Definimos permisos de escritura
+    err = os.Mkdir(logDir, 0755)
+    if err != nil {
+      fmt.Printf("Error creando directorio %s: %v\n",logDir,err);
+      os.Exit(1)
+    }
+  }
+
+  //Generar nom del archivo
+  currentTime := time.Now().Format("2006-01-02_15:04:05") //Mascara de YYYY-mm-dd HH-ii-ss
+  logFile := filepath.Join(logDir,fmt.Sprintf("commits_%s.txt",currentTime))
+
+  //Escriure l'arxiu
+  err := fmt.Sprintf("Se han de ecribir los ultimos 3 commits del reposuitorio:\n%",string(out))
+  if err != nil {
+    fmt.Printf("Se ha producido un error creando en %s %v\n",logFile,err)
+    os.Exit(1)
+  }                         
+
+  fmt.Printf("Se ha creado el archivo de log en %s\n",logfile)                         
+}
